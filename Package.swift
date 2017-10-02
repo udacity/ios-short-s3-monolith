3b2fa78207865aad4324c4f8062a871a0af0e2e0
Jarrod Parkes <parkesfjarrod@gmail.com>
2017-10-02 13:56:25 -0500

    use kitura image

--- Package.swift
+++ Package.swift
@@ -1,14 +1,16 @@
 // swift-tools-version:3.1
-
 import PackageDescription
 
 let package = Package(
     name: "monolith",
+    targets: [
+        Target(name: "monolith")
+    ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsHTTP.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 1)
     ]
 )
